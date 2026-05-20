75a384f16a60f8c910f83aa66293d0d3f5b2d299
awehero <147273991+awehero@users.noreply.github.com>
2026-05-20 18:13:06 -0400

    Update blocksData.js

--- blocksData.js
+++ blocksData.js
@@ -1,25 +1,31 @@
 const libraryData = {
-    quests: {
-        type: "folder",
-        children: {
-            gobstones: {
-                type: "folder",
-                name: "Gobs of Gobstones",
-                children: {
-                    start: { type: "block", name: "Start" },
-                    courtyard: { type: "block", name: "Courtyard" },
-                    tower: { type: "block", name: "Tower" },
-                    trophy: { type: "block", name: "Trophy Room" }
+    type: "folder",
+    name: "root",
+    children: {
+        quests: {
+            type: "folder",
+            name: "Quests",
+            children: {
+                gobstones: {
+                    type: "folder",
+                    name: "Gobs of Gobstones",
+                    children: {
+                        start: { type: "block", name: "Start" },
+                        courtyard: { type: "block", name: "Courtyard" },
+                        tower: { type: "block", name: "Tower" },
+                        trophy: { type: "block", name: "Trophy Room" }
+                    }
                 }
             }
-        }
-    },
+        },
 
-    travel: {
-        type: "folder",
-        children: {
-            floo1: { type: "block", name: "Floo: Hogwarts Castle" },
-            floo2: { type: "block", name: "Floo: Hogsmeade" }
+        travel: {
+            type: "folder",
+            name: "Travel",
+            children: {
+                floo1: { type: "block", name: "Floo: Hogwarts Castle" },
+                floo2: { type: "block", name: "Floo: Hogsmeade" }
+            }
         }
     }
 };
