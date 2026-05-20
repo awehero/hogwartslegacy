b02b4f25c9ff922caeb52af1917a871b9636ffaf
awehero <147273991+awehero@users.noreply.github.com>
2026-05-20 17:32:34 -0400

    initial

---
+++ blockData.js
@@ -0,0 +1,17 @@
+let libraryData = {
+    quests: [
+        {
+            id: "gobstones",
+            name: "Gobs of Gobstones",
+            steps: [
+                "Start",
+                "Transfiguration Courtyard",
+                "Hogwarts Grounds",
+                "Ravenclaw Tower",
+                "Divination",
+                "Trophy Room (2 gobstones)",
+                "Redeem"
+            ]
+        }
+    ]
+};
--- route_maker.js
+++ route_maker.js
@@ -1,15 +1,73 @@
 let library = document.getElementById("blockLibrary");
 let routeContainer = document.getElementById("routeContainer");
 
+function buildLibrary() {
+
+    for (let category in libraryData) {
+
+        let folder = document.createElement("div");
+        folder.className = "folder";
+
+        let header = document.createElement("div");
+        header.className = "folderHeader";
+        header.innerText = category;
+
+        let content = document.createElement("div");
+        content.className = "folderContent";
+
+        libraryData[category].forEach(block => {
+
+            let div = document.createElement("div");
+            div.className = "libraryBlock";
+
+            div.dataset.id = block.id;
+            div.dataset.type = category;
+
+            div.innerText = block.name;
+
+            content.appendChild(div);
+        });
+
+        folder.appendChild(header);
+        folder.appendChild(content);
+        library.appendChild(folder);
+    }
+}
+
+buildLibrary();
+
+function renderRouteBlock(type, block) {
+
+    let title = block.name || "Unknown";
+
+    let duration = block.duration || 0;
+
+    if (type == "quest") {
+        return `<b>${title}</b><br>Quest<br>${duration}s`;
+    }
+
+    if (type == "travel") {
+        return `<b>${title}</b><br>Travel<br>${duration}s`;
+    }
+
+    if (type == "item") {
+        return `<b>${title}</b><br>Item Pickup<br>${duration}s`;
+    }
+
+    if (type == "enemy") {
+        return `<b>${title}</b><br>Enemy Encounter<br>${duration}s`;
+    }
+
+    return `<b>${title}</b>`;
+}
+
 new Sortable(library, {
     group: {
         name: "shared",
         pull: "clone",
         put: false
     },
-
     sort: false,
-
     animation: 150
 });
 
@@ -24,48 +82,31 @@ new Sortable(routeContainer, {
     animation: 150,
 
     onAdd: function (evt) {
-
         let block = evt.item;
+        let id = block.dataset.id;
+
+        // find quest in library
+        let quest = libraryData.quests.find(q => q.id === id);
 
         block.classList.remove("libraryBlock");
         block.classList.add("routeBlock");
 
-        let type = block.dataset.type;
+        // clear original content
+        block.innerHTML = "";
 
-        if (type == "quest") {
+        // if it's a quest, expand it
+        if (quest) {
 
-            block.innerHTML = `
-                <b>Main Quest</b>
-                <br>
-                Duration: 300s
-            `;
-        }
+            let title = document.createElement("div");
+            title.innerHTML = "<b>" + quest.name + "</b>";
+            block.appendChild(title);
 
-        if (type == "travel") {
-
-            block.innerHTML = `
-                <b>Travel Segment</b>
-                <br>
-                Duration: 45s
-            `;
-        }
-
-        if (type == "item") {
-
-            block.innerHTML = `
-                <b>Item Pickup</b>
-                <br>
-                Duration: 10s
-            `;
-        }
-
-        if (type == "enemy") {
-
-            block.innerHTML = `
-                <b>Enemy Encounter</b>
-                <br>
-                Duration: 20s
-            `;
+            quest.steps.forEach(step => {
+                let stepDiv = document.createElement("div");
+                stepDiv.className = "stepBlock";
+                stepDiv.innerText = step;
+                block.appendChild(stepDiv);
+            });
         }
     }
 });
