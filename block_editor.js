72e6128791e4fada0db0a17081c0b5e5037d63de
awehero <147273991+awehero@users.noreply.github.com>
2026-06-16 15:46:46 -0400

    file system start

--- block_editor.js
+++ block_editor.js
@@ -3,11 +3,10 @@ function openBlockEditor(block){
     blockEditor.innerHTML=`
     <div>${block.dataset.path}</div>
     <label>Notes</label>
-    <textarea id="notes"></textarea>
     <label>Custom name</label>
     <input id="custom">
+    <textarea id="notes"></textarea>
     <button id="splitBtn">Split</button>
-    <button id="deleteBtn" style="margin-top:10px;background:#a33;color:white;">Delete</button>
     `;
 
     const notes=document.getElementById("notes");
