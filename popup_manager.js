0f516f671eb588aa8d811eb073e02abc35e6a0bf
awehero <147273991+awehero@users.noreply.github.com>
2026-06-25 20:21:43 -0400

    moving stuff around

--- popup_manager.js
+++ popup_manager.js
@@ -34,25 +34,6 @@ function displayRoutes() {
     let sorted = Object.values(store.saves).sort((a, b) => b.timestamp - a.timestamp);
     let html = "";
     sorted.forEach(save => {
-        /*html += `
-        <div class="routeDisplayContainer">
-            <div class="routeDisplayLeft">
-                <div class="routeDisplayTitle">${save.title}</div>
-                <div class="routeDisplayCount">${save.route.length == 1 ? "1 Block" : save.route.length + " Blocks"}</div>
-            </div>
-            <div class="routeDisplayMiddle">
-                <div class="routeDisplayDate">${formatDate(save.timestamp)}</div>
-                <div class="routeDisplayId">${save.id}</div>
-            </div>
-            <div class="routeDisplayRight">
-                <button data-id="${save.id}" class="routeDisplayButton openRoute" onclick="routeDisplayFunction('openRoute', '${save.id}')">Open Route</button>
-                <button data-id="${save.id}" class="routeDisplayButton exportRoute" onclick="routeDisplayFunction('exportRoute', '${save.id}')">Export Route</button>
-                <button data-id="${save.id}" class="routeDisplayButton exportNotes" onclick="routeDisplayFunction('exportNotes', '${save.id}')">Export Notes</button>
-                <button data-id="${save.id}" class="routeDisplayButton copyNotes" onclick="routeDisplayFunction('copyNotes', '${save.id}')">Copy Notes</button>
-                <button data-id="${save.id}" class="routeDisplayButton deleteRoute" onclick="routeDisplayFunction('deleteRoute', '${save.id}')">Delete Route</button>
-            </div>
-        </div>
-        `;*/
         html += `
         <div class="routeDisplayContainer">
             <div class="routeDisplayLeft">
@@ -62,9 +43,9 @@ function displayRoutes() {
                 <div class="routeDisplayId">${save.id}</div>
             </div>
             <div class="routeDisplayMiddle">
+                <button data-id="${save.id}" class="routeDisplayButton openRoute" onclick="routeDisplayFunction('openRoute', '${save.id}')">Open Route</button>
             </div>
             <div class="routeDisplayRight">
-                <button data-id="${save.id}" class="routeDisplayButton openRoute" onclick="routeDisplayFunction('openRoute', '${save.id}')">Open Route</button>
                 <button data-id="${save.id}" class="routeDisplayButton exportRoute" onclick="routeDisplayFunction('exportRoute', '${save.id}')">Export Route</button>
                 <button data-id="${save.id}" class="routeDisplayButton exportNotes" onclick="routeDisplayFunction('exportNotes', '${save.id}')">Export Notes</button>
                 <button data-id="${save.id}" class="routeDisplayButton copyNotes" onclick="routeDisplayFunction('copyNotes', '${save.id}')">Copy Notes</button>
